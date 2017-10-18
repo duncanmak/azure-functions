d3736390e23eee82604ab8966a6bbb846dc332de
Duncan Mak <duncan.mak@microsoft.com>
2017-10-18 18:28:57 -0400

    Initial commit

---
+++ Hello/Hello.fs
@@ -0,0 +1,9 @@
+﻿module Hello
+
+open Microsoft.Azure.WebJobs.Host
+open System.Net
+open System.Net.Http
+
+let Run(req: HttpRequestMessage, log: TraceWriter) =
+  log.Info(sprintf "F# HTTP trigger function processed a request.")
+  req.CreateResponse(HttpStatusCode.OK, "Hello World!")
